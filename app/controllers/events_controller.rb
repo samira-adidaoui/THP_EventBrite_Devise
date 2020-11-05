71ada8be38a3356af2a95e94e888a5ebf4b4fa37
Samira Adidaoui <samira.adidaoui@gmail.com>
2020-11-05 23:50:03 +0100

    Mailer

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,7 @@
+class EventsController < ApplicationController
+
+  def index
+    @event = Event.all
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   #validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
